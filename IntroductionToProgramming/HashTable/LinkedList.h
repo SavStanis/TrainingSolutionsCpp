af808f2ed5e45109b2f86062a966e5395c6f7c10
SavStanis <42719022+SavStanis@users.noreply.github.com>
2019-03-16 21:37:24 +0200

    Realization of list

--- IntroductionToProgramming/HashTable/LinkedList.h
+++ IntroductionToProgramming/HashTable/LinkedList.h
@@ -8,11 +8,13 @@ private:
 	class Node
 	{
 	public:
+		T name;
 		T value;
 		Node<T>* next;
 		Node<T>* prev;
-		Node(T value)
+		Node(T name, T value)
 		{
+			this->name = name;
 			this->value = value;
 			next = nullptr;
 			prev = nullptr;
@@ -20,11 +22,60 @@ private:
 	};
 	Node<T>* head;
 	Node<T>* end;
+	int size;
 public:
 	LinkedList();
-	void push(T value);
-	T pop();
-	void isEmpty();
+	void push (T name, T value);
+	T find(T name);
+	bool isEmpty();
 	int getSize();
 };
 
+template<typename T>
+LinkedList<T>::LinkedList()
+{
+	head = nullptr;
+	end = nullptr;
+	size = 0;
+}
+
+template<typename T>
+void LinkedList<T>::push(T name, T value)
+{
+	Node<T>* newNode = new Node<T>(name, value);
+	if (size == 0)
+	{
+		head = newNode;
+		end = newNode;
+	}
+	else
+	{
+		head->next = newNode;
+		newNode->prev = head;
+		head = newNode;
+	}
+	size++;
+}
+
+template<typename T>
+T LinkedList<T>::find(T name)
+{
+	Node<T>* element = end;
+	while (element != nullptr && element->name != name)
+	{
+		element = element->next;
+	}
+	return element->value;
+}
+
+template<typename T> 
+bool LinkedList<T>::isEmpty()
+{
+	return (size == 0) ? true : false;
+}
+
+template<typename T>
+int LinkedList<T>::getSize()
+{
+	return size;
+}
